0d09b0e8be5b5cf2a8577ff3be4f089d23a1a99d
Solomon Rajkumar <solomonrajkumar.sathianandaraj@mckesson.com>
2025-11-24 22:38:32 -0700

    week 5 data science Univariate Analysis

---
+++ Week-5-DataScience/Univariate/Univariate.py
@@ -0,0 +1,13 @@
+class Univariate:
+    def __init__(self, dataframe):
+        self.dataframe = dataframe
+
+    def quanQual(self):
+        quantitative = []
+        qualitative = []
+        for column in self.dataframe.columns:
+            if self.dataframe[column].dtypes == 'object':
+                qualitative.append(column)
+            else:
+                quantitative.append(column)
+        return quantitative, qualitative
